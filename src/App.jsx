588f3121fbae9ab933a4e951a473bfc1e8759d3b
Yassine Louimni <yassinos.louimnis@gmail.com>
2024-11-26 15:56:17 +0100

    Home page Updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,27 @@ import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import Features from './Components/Features/Features'
-
-
+import Testimonials from './Components/Testimonials/Testimonials'
+import Contact from './Components/Contact/Contact'
+import Footer from './Components/Footer/Footer'
+import Title from './Components/Title/Title'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <div className="container">
+       <Title subtitle='Our Features' title='What We Offer'/>
+       <Features/>
+       <Title subtitle='TESTIMONIALS' title='What People Says'/>
+       <Testimonials/>
+       <Title subtitle='Contact Us' title='Get in Touch'/>
+       <Contact/>
+       <Footer/>
+      </div>
     </div>
+  
   )
 }
 
